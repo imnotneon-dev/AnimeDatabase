242083492487b2a147e1dec9d763f024ba296c21
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-09-30 16:20:16 +0800

    added initial .sql files - create_schema contains all CREATE commands for both Core and Transaction records - add_constraints are extra modifications to the tables, primarily adding FOREIGN KEY - data_* contains the initial data for the Core Records

---
+++ resources/sql/01_create_schema.sql
@@ -0,0 +1,11 @@
+-- ALL CORE AND TRANSACTION TABLES WILL BE CREATED HERE WITH RESPECTIVE
+-- PRIMARY KEY FIRST
+-- FOREIGN KEYS TO BE ADDED IN CONSTRAINTS FILE
+-- since you cannot add a foreign key if it is not yet generated
+
+-- CORE
+-- User Table
+-- etc etc just comment it HAHAHAH
+
+
+-- TRANSACTION

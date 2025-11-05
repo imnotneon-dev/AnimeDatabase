e1043bd18363d50ce1c263187c9204b5e784bd25
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-05 22:34:39 +0800

    feat: Added account panel base

---
+++ src/com/anime/view/AccountPanel.java
@@ -0,0 +1,16 @@
+package com.anime.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AccountPanel extends JPanel{
+
+    public AccountPanel() {
+        init();
+    }
+    private void init() {
+        setOpaque(true);
+        setVisible(true);
+        setBackground(Color.CYAN);
+    }
+}

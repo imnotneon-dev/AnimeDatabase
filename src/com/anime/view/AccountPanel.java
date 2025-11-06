84e743f100c99a816ed34f5f6978b45be174c211
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-06 17:42:16 +0800

    fix: Fixed alignment issues of components and container panel
    Components are now properly centered inside the container panel, which is centered inside the main panel.

--- src/com/anime/view/AccountPanel.java
+++ src/com/anime/view/AccountPanel.java
@@ -6,11 +6,13 @@ import java.awt.*;
 public class AccountPanel extends JPanel{
     private JButton loginBtn = new JButton();
     private JButton signupBtn = new JButton();
-    private JTextField loginNameField = new JTextField();
+    private JTextField loginNameField = new JTextField(25);
     private JPanel container = new JPanel();
     private GridBagLayout layout = new GridBagLayout();
     private final Component LEFT_GLUE = Box.createHorizontalGlue();
     private final Component RIGHT_GLUE = Box.createHorizontalGlue();
+    private final Component TOP_GLUE = Box.createVerticalGlue();
+    private final Component BOT_GLUE = Box.createVerticalGlue();
 
     public AccountPanel() {
         init();
@@ -21,26 +23,42 @@ public class AccountPanel extends JPanel{
         setBackground(Color.CYAN);
         setLayout(layout);
 
+
         initComponents();
     }
 
     private void initComponents(){
-//        container.setPreferredSize(new Dimension(100,100));
-//        container.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
-        container.setLayout(new BoxLayout(container,BoxLayout.PAGE_AXIS));
-
+        // GridBagLayout centers the container panel
         layout.setConstraints(container, new GridBagConstraints());
 
+        // set layout of component container so components are centered
+        container.setLayout(new BoxLayout(container,BoxLayout.Y_AXIS));
+
+        // Set text and component attributes
         loginNameField.setText("Enter your account");
         loginBtn.setText("Login");
         loginBtn.setFocusable(true);
-
         signupBtn.setText("Signup");
 
-        container.add(loginNameField);
-        container.add(loginBtn);
-        container.add(signupBtn);
+        // Set component alignments
+        loginNameField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        signupBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        // Setup visual appearance of container panel
+        container.add(TOP_GLUE);
+
+        container.add(Box.createVerticalStrut(5));
+        container.add(loginNameField);
+        container.add(Box.createVerticalStrut(10));
+        container.add(loginBtn);
+        container.add(Box.createVerticalStrut(5));
+        container.add(signupBtn);
+        container.add(Box.createVerticalStrut(5));
+
+        container.add(BOT_GLUE);
+
+        // Add to main panel
         add(container);
     }
 

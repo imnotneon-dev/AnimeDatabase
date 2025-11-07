a2fedb9deaef79bc13b0015fa020bc1ba905638b
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-07 22:16:45 +0800

    feat: Added scroll pane, enclosing sub-panels
    Scroll pane added to add emphasis to series information and possibly background. Episode information are at the bottom of the scrolldown. Panel sizes were also enlarged to accomodate

--- src/com/anime/view/SeriesPage.java
+++ src/com/anime/view/SeriesPage.java
@@ -8,22 +8,26 @@ public class SeriesPage extends JPanel{
     private JButton button = new JButton("SeriesPage");
     private JPanel seriesInfoPnl = new JPanel();
     private JPanel episodesPnl = new JPanel();
+    private JPanel contentPnl = new JPanel();
+    private JScrollPane scrollPane = new JScrollPane(contentPnl);
     private JLabel titleLb = new JLabel("My Hero Aca");
     private JLabel genreLb = new JLabel("Action");
     private JLabel releaseYearLb = new JLabel("2025-09-08");
     private JLabel epCountLb = new JLabel("5");
     private JLabel statusLb = new JLabel("Ongoing");
 //    private List<Episode> episodeList;
-    private List<String> episodeList = List.of("1","2","3","4","5");
+    private List<String> episodeList = List.of(
+            "Episode 1","Episode 2","Episode 3","Episode 4","Episode 5",
+        "Episode 6","Episode 7","Episode 8","Episode 9","Episode 10",
+        "Episode 11","Episode 12");
     private GridBagLayout gb = new GridBagLayout();
     /*public SeriesPage (Series series, List<Episodes> epList){
         // take the info from the series passed then use that to display stuff
-        this.seriesTitle = series.title;
-        this.seriesGenre = series.genre;
-        this.releaseYear = series.rel_year;
-        this.episodeCount = series.ep_count;
-        this.seriesStatus = series.status;
-        this.episodeList = epList;
+        titleLb.setText();
+        genreLb.setText();
+        releaseYearLb.setText();
+        epCountLb.setText();
+        statusLb.setText();
     }*/
 
     public SeriesPage() {
@@ -33,7 +37,7 @@ public class SeriesPage extends JPanel{
         setOpaque(true);
         setVisible(true);
         setBackground(Color.BLUE);
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         initComponents();
     }
@@ -41,8 +45,8 @@ public class SeriesPage extends JPanel{
     private void initComponents(){
         seriesInfoPnl.setLayout(gb);
         seriesInfoPnl.setBackground(Color.yellow);
-        seriesInfoPnl.setPreferredSize(new Dimension(1280,450));
-        seriesInfoPnl.setMaximumSize(new Dimension(1280,600));
+        seriesInfoPnl.setPreferredSize(new Dimension(1280,700));
+//        seriesInfoPnl.setMaximumSize(new Dimension(1280,600));
 //        seriesInfoPnl.setPreferredSize(new Dimension(420,720));
 //        seriesInfoPnl.setMaximumSize(new Dimension(500, 720));
         seriesInfoPnl.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -52,11 +56,10 @@ public class SeriesPage extends JPanel{
         seriesInfoPnl.add(epCountLb);
         seriesInfoPnl.add(statusLb);
 
-
-        episodesPnl.setLayout(new FlowLayout(FlowLayout.LEFT));
+        episodesPnl.setLayout(new FlowLayout(FlowLayout.CENTER));
         episodesPnl.setBackground(Color.black);
-        seriesInfoPnl.setPreferredSize(new Dimension(1280,270));
-        seriesInfoPnl.setMaximumSize(new Dimension(1280,300));
+        seriesInfoPnl.setPreferredSize(new Dimension(1280,720));
+//        seriesInfoPnl.setMaximumSize(new Dimension(1280,300));
 //        episodesPnl.setPreferredSize(new Dimension(100,100));
         episodesPnl.setAlignmentX(Component.CENTER_ALIGNMENT);
         for(String s: episodeList){
@@ -66,9 +69,16 @@ public class SeriesPage extends JPanel{
             episodesPnl.add(series);
         }
 
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setWheelScrollingEnabled(true);
+        scrollPane.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
 
-        add(seriesInfoPnl);
-        add(episodesPnl);
+        contentPnl.setLayout(new BoxLayout(contentPnl,BoxLayout.Y_AXIS));
+        contentPnl.add(seriesInfoPnl);
+        contentPnl.add(episodesPnl);
+        add(scrollPane);
         
     }
     public JButton getButton(){

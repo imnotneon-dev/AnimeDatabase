308b15373cbd9eaf39753c0ed16b6fc9d5813836
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-08 15:25:36 +0800

    feat: Added actor roles-related components
    For now, button will be used as placeholders but in the future, it should hopefully be its own "card"

--- src/com/anime/view/ActorPage.java
+++ src/com/anime/view/ActorPage.java
@@ -1,7 +1,9 @@
 package com.anime.view;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.List;
 
 public class ActorPage extends JPanel{
     private JPanel actorInfoPnl = new JPanel();
@@ -11,7 +13,9 @@ public class ActorPage extends JPanel{
     private JLabel ageLb = new JLabel("40");
     private JLabel placeOfBirthLb = new JLabel("Tokyo");
     private JLabel agencyNameLb = new JLabel("Agency");
+    private JScrollPane scrollPane = new JScrollPane(rolesPnl);
 //    private List<ActorRole> rolesList;
+    private List<String> rolesList = List.of("Bakugo","Midoriya", "Todoroki");
     /*
     * public ActorPage(Actor actor, List<ActorRole> roles){
     *   actorPhoto.
@@ -41,9 +45,12 @@ public class ActorPage extends JPanel{
         actorInfoPnl.setAlignmentY(Component.CENTER_ALIGNMENT);
 //        actorInfoPnl.setMinimumSize(new Dimension(1280/2,720));
         actorInfoPnl.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // fix this line
+        actorInfoPnl.setBorder(new EmptyBorder(10,10,10,10));
 
         actorInfoPnl.add(Box.createVerticalGlue());
         actorInfoPnl.add(Box.createHorizontalGlue());
+
         actorPhoto.setAlignmentX(Component.CENTER_ALIGNMENT);
         actorInfoPnl.add(actorPhoto);
         nameLb.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -54,10 +61,35 @@ public class ActorPage extends JPanel{
         actorInfoPnl.add(placeOfBirthLb);
         agencyNameLb.setAlignmentX(Component.CENTER_ALIGNMENT);
         actorInfoPnl.add(agencyNameLb);
+
         actorInfoPnl.add(Box.createHorizontalGlue());
         actorInfoPnl.add(Box.createVerticalGlue());
 
+        rolesPnl.setLayout(new BoxLayout(rolesPnl, BoxLayout.Y_AXIS));
+        rolesPnl.setBackground(Color.black);
+        rolesPnl.setPreferredSize(new Dimension(1280/2, 720));
+        rolesPnl.setMaximumSize(new Dimension(1280/2, 720));
+        rolesPnl.setAlignmentY(Component.CENTER_ALIGNMENT);
+//        rolesPnl.setMinimumSize(new Dimension(1280/2,720));
+        rolesPnl.setAlignmentX(Component.LEFT_ALIGNMENT);
+        rolesPnl.setBorder(new EmptyBorder(10,10,10,10));
+
+        for(String r: rolesList){
+            JLabel role = new JLabel();
+            role.setText(r);
+            role.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+            rolesPnl.add(role);
+        }
+
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setWheelScrollingEnabled(true);
+        scrollPane.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+
         add(actorInfoPnl);
+        add(scrollPane);
 
     }
 }

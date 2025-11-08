218c13c71d2b44c86eedc404b7b19f28785c466a
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-08 16:43:58 +0800

    added comment re variable name

--- resources/sql/data_likedepisodes.sql
+++ resources/sql/data_likedepisodes.sql
@@ -4,7 +4,7 @@ CREATE TABLE LikedEpisode(
   date_added DATE DEFAULT CURRENT_DATE,
   PRIMARY KEY (user_id, episode_id),
   FOREIGN KEY (user_id) REFERENCES Users(user_id),
-  FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id)
+  FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id) --episode_id variable name depends on what variable name will be used in the episodes.sql file
 );
 
 

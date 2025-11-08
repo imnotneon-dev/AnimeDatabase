7115e0fc255d08904b74f25f211c7cee3d3bed2b
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-08 15:09:43 +0800

    feat: Added initial components to the actor page
    Panel and components related to the actor's personal information was added

--- src/com/anime/view/ActorPage.java
+++ src/com/anime/view/ActorPage.java
@@ -4,7 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ActorPage extends JPanel{
-    private JButton button = new JButton("SeriesPage");
+    private JPanel actorInfoPnl = new JPanel();
+    private JPanel rolesPnl = new JPanel();
+    private JLabel actorPhoto = new JLabel();
+    private JLabel nameLb = new JLabel("Name");
+    private JLabel ageLb = new JLabel("40");
+    private JLabel placeOfBirthLb = new JLabel("Tokyo");
+    private JLabel agencyNameLb = new JLabel("Agency");
+//    private List<ActorRole> rolesList;
+    /*
+    * public ActorPage(Actor actor, List<ActorRole> roles){
+    *   actorPhoto.
+    *   nameLb.setText(actor.name);
+    *   ageLb.setText();
+    *   placeOfBirthLb.setText();
+    *   agencyNameLb.setText();
+    *   rolesList = roles;
+    * }
+    * */
     public ActorPage() {
         init();
     }
@@ -12,11 +29,35 @@ public class ActorPage extends JPanel{
         setOpaque(true);
         setVisible(true);
         setBackground(Color.BLUE);
-        setLayout(new FlowLayout());
-        add(button);
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        initComponents();
     }
 
-    public JButton getButton(){
-        return button;
+    private void initComponents(){
+        actorInfoPnl.setLayout(new BoxLayout(actorInfoPnl,BoxLayout.Y_AXIS));
+        actorInfoPnl.setBackground(Color.gray);
+        actorInfoPnl.setPreferredSize(new Dimension(1280/2, 720));
+        actorInfoPnl.setMaximumSize(new Dimension(1280/2, 720));
+        actorInfoPnl.setAlignmentY(Component.CENTER_ALIGNMENT);
+//        actorInfoPnl.setMinimumSize(new Dimension(1280/2,720));
+        actorInfoPnl.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        actorInfoPnl.add(Box.createVerticalGlue());
+        actorInfoPnl.add(Box.createHorizontalGlue());
+        actorPhoto.setAlignmentX(Component.CENTER_ALIGNMENT);
+        actorInfoPnl.add(actorPhoto);
+        nameLb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        actorInfoPnl.add(nameLb);
+        ageLb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        actorInfoPnl.add(ageLb);
+        placeOfBirthLb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        actorInfoPnl.add(placeOfBirthLb);
+        agencyNameLb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        actorInfoPnl.add(agencyNameLb);
+        actorInfoPnl.add(Box.createHorizontalGlue());
+        actorInfoPnl.add(Box.createVerticalGlue());
+
+        add(actorInfoPnl);
+
     }
 }

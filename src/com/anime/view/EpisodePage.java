231538ac13d46e8cc330cb295edb3c3c542284a7
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-14 08:29:45 +0800

    add: Added custom review cards and review text area
    The ReviewCards contain the username and their review, which is placed in a scrollpane. The text area (where they can type/draft comments/reviews) is also in a scrollable area.

--- src/com/anime/view/EpisodePage.java
+++ src/com/anime/view/EpisodePage.java
@@ -1,5 +1,7 @@
 package com.anime.view;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -8,16 +10,20 @@ public class EpisodePage extends JPanel{
     private JButton button = new JButton("SeriesPage");
     private JPanel videoPanel = new JPanel();
     private JPanel episodeInfoPnl = new JPanel();
-    private JPanel commentsPnl = new JPanel();
-    private JPanel infoPnl = new JPanel();
-    private JScrollPane scrollPane= new JScrollPane(infoPnl);
     private JLabel titleLb = new JLabel("Episode Title");
     private JLabel seriesLb = new JLabel("Series Title");
-    private JLabel sypnosisLb = new JLabel("Sypnosis");
-    private JLabel releaseDataLb = new JLabel("Rlease Date");
-    private JLabel runtimeLb = new JLabel("runtime");
-
+    private List<ReviewCard> reviews = List.of(new ReviewCard("user1", "review 1 wowee"),
+                                        new ReviewCard("user2",
+                                                "jeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdsh" +
+                                                        "jeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdsh" +
+                                                        "jeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdsh" +
+                                                        "jeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdsh" +
+                                                        "jeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdshjeladhsalkdsh"));
+    private JLabel commentsLb = new JLabel("Comments");
+    private JTextArea reviewTextArea = new JTextArea(3, 15);
     private JPanel reviewsPanel = new JPanel();
+    private JButton submitCommentBtn = new JButton("Submit");
+    private JScrollPane reviewTextAreaScroll = new JScrollPane(reviewTextArea);
     private JScrollPane reviewsScrollPane = new JScrollPane(reviewsPanel);
     
     public EpisodePage() {
@@ -35,57 +41,81 @@ public class EpisodePage extends JPanel{
         videoPanel.setLayout(new BoxLayout(videoPanel,BoxLayout.Y_AXIS));
         videoPanel.setBackground(Color.black);
         videoPanel.setPreferredSize(new Dimension(1280, 550));
-        videoPanel.setMaximumSize(new Dimension(1280, 550));
-
+        videoPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 550));
         videoPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
-
 
         episodeInfoPnl.setLayout(new BoxLayout(episodeInfoPnl,BoxLayout.Y_AXIS));
         episodeInfoPnl.setBackground(Color.gray);
-        episodeInfoPnl.setPreferredSize(new Dimension(Integer.MAX_VALUE, 230));
-        episodeInfoPnl.setMaximumSize(new Dimension(Integer.MAX_VALUE, 230));
-        episodeInfoPnl.setAlignmentY(Component.CENTER_ALIGNMENT);
-//        episodeInfoPnl.setMinimumSize(new Dimension(1280/2,720));
+        episodeInfoPnl.setPreferredSize(new Dimension(1300, 80));
+        episodeInfoPnl.setMaximumSize(new Dimension(Integer.MAX_VALUE, 80));
         episodeInfoPnl.setAlignmentX(Component.CENTER_ALIGNMENT);
-//        episodeInfoPnl.setBorder(new EmptyBorder(10,10,10,10));
+        episodeInfoPnl.setBorder(new EmptyBorder(20,30,20,20));
         episodeInfoPnl.add(titleLb);
         episodeInfoPnl.add(seriesLb);
-        episodeInfoPnl.add(sypnosisLb);
-        episodeInfoPnl.add(releaseDataLb);
 
+        /**
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setWheelScrollingEnabled(true);
-        scrollPane.setAlignmentY(Component.CENTER_ALIGNMENT);
-        episodeInfoPnl.setPreferredSize(new Dimension(1280, 230));
-        episodeInfoPnl.setMaximumSize(new Dimension(1300, 230));
-//        scrollPane.setBorder(BorderFactory.createEmptyBorder());
         scrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(1,Integer.MAX_VALUE));
+        scrollPane.setBorder(new EmptyBorder(0,0,0,0));
+         */
+        reviewTextArea.setLineWrap(true);
+        reviewTextArea.setWrapStyleWord(true);
+        reviewTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+        reviewTextArea.setMaximumSize(new Dimension(Integer.MAX_VALUE, reviewTextArea.getPreferredSize().height));
+
+        reviewTextAreaScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        reviewTextAreaScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        reviewTextAreaScroll.setWheelScrollingEnabled(true);
+        reviewTextAreaScroll.setMaximumSize(new Dimension(Integer.MAX_VALUE, reviewTextArea.getPreferredSize().height));
+        reviewTextAreaScroll.setAlignmentX(Component.CENTER_ALIGNMENT);
+        reviewTextAreaScroll.getVerticalScrollBar().setUnitIncrement(16);
+//        reviewTextAreaScroll.getVerticalScrollBar().setPreferredSize(new Dimension(1,Integer.MAX_VALUE));
+        reviewTextAreaScroll.setBorder(new EmptyBorder(10, 20, 10, 45));
+        reviewTextAreaScroll.setBorder(BorderFactory.createTitledBorder("Reviews"));
+        reviewTextAreaScroll.setBorder(BorderFactory.createEmptyBorder());
 
         reviewsPanel.setLayout(new BoxLayout(reviewsPanel, BoxLayout.Y_AXIS));
         reviewsPanel.setBackground(Color.lightGray);
-        reviewsPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        reviewsPanel.setPreferredSize(new Dimension(1280, 600)); 
-        reviewsPanel.setMaximumSize(new Dimension(1400, 600));  
+        reviewsPanel.setBorder(new EmptyBorder(10, 20, 10, 45));
+        reviewsPanel.setPreferredSize(new Dimension(1280, 600));
+        reviewsPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 720));
 
-        // checking reviews onleh
-        addReview("Great episode!");
-        addReview("Animation was amazing this week.");
-        addReview("WOWOWOWOWOWOW");
+        reviewsPanel.add(new JLabel("Leave Comment"));
+        reviewsPanel.add(Box.createVerticalStrut(5));
+        reviewsPanel.add(reviewTextAreaScroll);
+        reviewsPanel.add(Box.createVerticalStrut(5));
+        reviewsPanel.add(submitCommentBtn);
+        reviewsPanel.add(Box.createVerticalStrut(5));
+
+
+        reviewsPanel.add(commentsLb);
+        reviewsPanel.add(Box.createVerticalStrut(10));
+        for(ReviewCard r: reviews){
+            ReviewCard revCard = r;
+            revCard.setAlignmentX(Component.LEFT_ALIGNMENT);
+            reviewsPanel.add(revCard);
+            reviewsPanel.add(Box.createVerticalStrut(10));
+        }
 
         reviewsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         reviewsScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         reviewsScrollPane.setWheelScrollingEnabled(true);
-        reviewsScrollPane.setPreferredSize(new Dimension(1500, 230));
+        reviewsScrollPane.setPreferredSize(new Dimension(1280, 230));
+        reviewsScrollPane.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
         reviewsScrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
-        reviewsScrollPane.setAlignmentY(Component.CENTER_ALIGNMENT);
+        reviewsScrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        reviewsScrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(1,Integer.MAX_VALUE));
         reviewsScrollPane.setBorder(BorderFactory.createTitledBorder("Reviews"));
+        reviewsScrollPane.setBorder(BorderFactory.createEmptyBorder());
 
-        infoPnl.add(episodeInfoPnl);
-        infoPnl.add(commentsPnl);
         add(videoPanel);
-        add(scrollPane);
+        add(episodeInfoPnl);
         add(reviewsScrollPane);
+        add(Box.createVerticalGlue());
     }
 
     public void addReview(String reviewText) {

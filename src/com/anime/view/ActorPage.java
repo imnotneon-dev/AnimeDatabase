5e4095e14fe27180bb83d0775230b958bd595e0c
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-14 19:14:25 +0800

    add: Added RoleCards for the role display panel

--- src/com/anime/view/ActorPage.java
+++ src/com/anime/view/ActorPage.java
@@ -15,7 +15,28 @@ public class ActorPage extends JPanel{
     private JLabel agencyNameLb = new JLabel("Agency");
     private JScrollPane rolesScrollPane = new JScrollPane(rolesPnl);
 //    private List<ActorRole> rolesList;
-    private List<String> rolesList = List.of("Bakugo","Midoriya", "Todoroki");
+    private List<RoleCard> rolesList = List.of(
+            new RoleCard("My Hero Academia: Final Season", "Katsuki Bakugo"),
+            new RoleCard("My Hero Academia: 3rd Season", "Katsuki Bakugo"),
+            new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+            new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+            new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+            new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia: 3rd Season", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia: 3rd Season", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia: 3rd Season", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"),
+        new RoleCard("My Hero Academia", "Katsuki Bakugo"));
     /*
     * public ActorPage(Actor actor, List<ActorRole> roles){
     *   actorPhoto.
@@ -39,6 +60,9 @@ public class ActorPage extends JPanel{
     }
 
     private void initComponents(){
+        JLabel filmography = new JLabel("Filmography");
+        filmography.setAlignmentX(Component.LEFT_ALIGNMENT);
+        filmography.setForeground(Color.white);
         actorInfoPnl.setLayout(new BoxLayout(actorInfoPnl,BoxLayout.Y_AXIS));
         actorInfoPnl.setBackground(Color.gray);
         actorInfoPnl.setPreferredSize(new Dimension((int)(1280/2.5), 720));
@@ -68,26 +92,28 @@ public class ActorPage extends JPanel{
 
         rolesPnl.setLayout(new BoxLayout(rolesPnl, BoxLayout.Y_AXIS));
         rolesPnl.setBackground(Color.black);
-        rolesPnl.setPreferredSize(new Dimension(1280, 720));
-        rolesPnl.setMaximumSize(new Dimension(1280, 720));
+//        rolesPnl.setPreferredSize(new Dimension(1280, 720));
+//        rolesPnl.setMaximumSize(new Dimension(1280, 720));
         rolesPnl.setAlignmentY(Component.CENTER_ALIGNMENT);
 //        rolesPnl.setMinimumSize(new Dimension(1280/2,720));
         rolesPnl.setAlignmentX(Component.LEFT_ALIGNMENT);
         rolesPnl.setBorder(new EmptyBorder(10,10,10,10));
 
-        for(String r: rolesList){
-            JLabel role = new JLabel();
-            role.setText(r);
+        rolesPnl.add(filmography);
+        rolesPnl.add(Box.createVerticalStrut(10));
+        for(RoleCard r: rolesList){
+            RoleCard role = r;
             role.setAlignmentX(Component.LEFT_ALIGNMENT);
-
             rolesPnl.add(role);
+            rolesPnl.add(Box.createVerticalStrut(10));
         }
 
         rolesScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         rolesScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         rolesScrollPane.setWheelScrollingEnabled(true);
-        rolesScrollPane.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        rolesScrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
         rolesScrollPane.setBorder(BorderFactory.createEmptyBorder());
+        rolesPnl.setBorder(new EmptyBorder(10,10,10,10));
 
         add(actorInfoPnl, BorderLayout.WEST);
         add(rolesScrollPane, BorderLayout.CENTER);

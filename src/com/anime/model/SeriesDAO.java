ff345d3b67b7e998b024481415b82f0dc5ea77d5
LilianV <rhaizza_legaspi@dlsu.edu.ph>
2025-11-15 23:25:45 +0800

    getSeriesById plng po

--- src/com/anime/model/SeriesDAO.java
+++ src/com/anime/model/SeriesDAO.java
@@ -9,6 +9,35 @@ public class SeriesDAO{
 
     private final Connection conn;
 
-    
+    public Series getSeriesById(int series_id) {
+        String sql = "SELECT * FROM series_t WHERE series_id = ?";
 
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, series_id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Series(
+                    rs.getInt("series_id"),
+                    rs.getString("title"),
+                    rs.getString("genre"),
+                    rs.getInt("release_year"),
+                    rs.getInt("total_episode_count"),
+                    rs.getString("status_of_series")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    
+    
 }
+
+
+

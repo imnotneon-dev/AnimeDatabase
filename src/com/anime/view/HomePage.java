40eed77580b686238127ad95e399e847a236c561
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-15 08:44:02 +0800

    add: Added implementation of horizontal shelves

--- src/com/anime/view/HomePage.java
+++ src/com/anime/view/HomePage.java
@@ -8,8 +8,12 @@ import java.awt.*;
 import java.util.List;
 
 public class HomePage extends JPanel{
-//    List<String> watchingList = List.of("Series1", "Series2");
-//    List<String> favoriteList = List.of("My Hero Academia", "Attack on Titan");
+    private List<SeriesCard> watchingList = List.of(
+            new SeriesCard("Haikyuu"), new SeriesCard("Blue Lock"), new SeriesCard("Battery Oblivion"));
+    private List<SeriesCard> favoriteList = List.of(
+            new SeriesCard("Haikyuu"), new SeriesCard("Blue Lock"), new SeriesCard("Battery Oblivion"),
+            new SeriesCard("Slam Dunk"), new SeriesCard("Ace of the Diamond"),
+            new SeriesCard("Inazuma Eleven"));
     private List<SeriesCard> catalog = List.of(
             new SeriesCard("Haikyuu"), new SeriesCard("Blue Lock"), new SeriesCard("Battery Oblivion"),
             new SeriesCard("Slam Dunk"), new SeriesCard("Ace of the Diamond"),
@@ -44,36 +48,41 @@ public class HomePage extends JPanel{
 //        gb.setConstraints(container, new GridBagConstraints());
         container.setLayout(new BoxLayout(container,BoxLayout.Y_AXIS));
         container.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
-//        JPanel watchShelf = createShelf("Watching", watchingList);
-//        JPanel favoriteShelf = createShelf("Favorite Shows", favoriteList);
+        JPanel watchShelf = createShelf("Watching", watchingList);
+        JPanel favoriteShelf = createShelf("Favorite Shows", favoriteList);
         JPanel shelf2 = createCatalog("Catalog", catalog);
-//        favoriteShelf.setBackground(Color.ORANGE);
+        favoriteShelf.setBackground(Color.ORANGE);
         shelf2.setBackground(Color.GRAY);
-//        container.add(watchShelf);
-//        container.add(favoriteShelf);
+        container.add(watchShelf);
+        container.add(favoriteShelf);
         container.add(shelf2);
 
         add(container);
     }
 
     private JPanel createShelf(String title, List<SeriesCard> seriesList){
+        JLabel seriesTitle = new JLabel(title);
         JPanel shelfPnl = new JPanel();
         JPanel seriesContentPnl = new JPanel();
         JScrollPane seriesContentScroller = new JScrollPane(seriesContentPnl);
-        JLabel seriesTitle = new JLabel(title);
+
+        seriesTitle.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         shelfPnl.setLayout(new BoxLayout(shelfPnl, BoxLayout.Y_AXIS));
         shelfPnl.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-        seriesContentPnl.setLayout(new FlowLayout(FlowLayout.LEFT,10,5));
+        seriesContentPnl.setLayout(new FlowLayout(FlowLayout.LEFT,15,10));
         seriesContentPnl.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-        seriesTitle.setAlignmentX(Component.LEFT_ALIGNMENT);
 
         seriesContentScroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         seriesContentScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
         seriesContentScroller.setWheelScrollingEnabled(true);
         seriesContentScroller.setAlignmentX(Component.LEFT_ALIGNMENT);
+        seriesContentScroller.getVerticalScrollBar().setUnitIncrement(10);
+        seriesContentScroller.getVerticalScrollBar().setPreferredSize(new Dimension(3,Integer.MAX_VALUE));
+
+        shelfPnl.setBorder(new EmptyBorder(10,10,15,10));
 
         for (SeriesCard s: seriesList){
 //            JButton series = new JButton();

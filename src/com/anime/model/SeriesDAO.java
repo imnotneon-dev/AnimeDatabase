e9958317312b1243e1e1e786f941f4e4d6ce2fad
LilianV <rhaizza_legaspi@dlsu.edu.ph>
2025-11-15 22:14:41 +0800

    starting <3

--- src/com/anime/model/SeriesDAO.java
+++ src/com/anime/model/SeriesDAO.java
@@ -1,3 +1,14 @@
+package com.anime.model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.anime.model.Series;
+
 public class SeriesDAO{
+
+    private final Connection conn;
+
     
+
 }

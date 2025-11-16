4ca2007fecaf2d439e6658c99d3adf0966679ea3
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-16 22:13:12 +0800

    add: Updated getters and setters for Episode attributes

--- src/com/anime/model/Episode.java
+++ src/com/anime/model/Episode.java
@@ -7,6 +7,7 @@ public class Episode {
     private String epTitle;
     private String sypnosis;
     private int runtime;
+    private int views;
     private LocalDate releaseDate;
     private int episodeId;
     private int seriesId;
@@ -24,27 +25,65 @@ public class Episode {
         return seriesId;
     }
 
+    public void setSeriesId(int seriesId){
+        this.seriesId = seriesId;
+    }
+
     public int getEpisodeId(){
         return episodeId;
+    }
+
+    public void setEpisodeId(int episodeId){
+        this.episodeId = episodeId;
     }
 
     public String getTitle(){
         return epTitle;
     }
 
+    public void setTitle(String title){
+        this.epTitle = title;
+    }
+
     public String getSypnosis(){
         return sypnosis;
+    }
+
+    public void setSypnosis(String sypnosis){
+        this.sypnosis = sypnosis;
     }
 
     public int getRuntime(){
         return runtime;
     }
 
+    public void setRuntime(int runtime){
+        this.runtime = runtime;
+    }
+
+    public int getViews(){
+        return views;
+    }
+
+    public void setViews(int views){
+        this.views = views;
+    }
+
     public LocalDate getReleaseDate(){
         return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate){
+        this.releaseDate = releaseDate;
     }
 
     public List<Review> getListOfReviews(){
         return listOfReviews;
     }
+
+    public void setListOfReviews(List<Review> listOfReviews){
+        this.listOfReviews = listOfReviews;
+    }
+
+
 }

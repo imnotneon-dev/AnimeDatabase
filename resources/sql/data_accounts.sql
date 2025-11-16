5af83aa6478b2a680cbccefcd6c27b9f255d3239
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-16 23:48:35 +0800

    added password to accounts sql

--- resources/sql/data_accounts.sql
+++ resources/sql/data_accounts.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Users(
   user_id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL,
   country VARCHAR(50),
   top_genre VARCHAR(50),
   date_user_created DATE DEFAULT CURRENT_DATE
 );
+

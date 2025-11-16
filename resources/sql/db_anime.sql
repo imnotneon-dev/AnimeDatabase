1e14f6838e8dc76dfc27ab1d0d29747873543636
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-16 22:17:05 +0800

    chore: reorganized sql create statements

--- resources/sql/db_anime.sql
+++ resources/sql/db_anime.sql
@@ -63,7 +63,7 @@ CREATE TABLE series_episodes_t (
 DROP TABLE IF EXISTS watchHistory;
 CREATE TABLE watchHistory (
     watch_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    username VARCHAR(50) NOT NULL,
     episode_id INT NOT NULL,
     watch_date DATE DEFAULT CURRENT_DATE,
     FOREIGN KEY (user_id) REFERENCES users(user_id),

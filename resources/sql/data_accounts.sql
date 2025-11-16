9afddea55671f2cccf3499aec88ef60fc0a7d9d0
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-16 23:49:20 +0800

    username is pk

--- resources/sql/data_accounts.sql
+++ resources/sql/data_accounts.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Users(
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(50) UNIQUE NOT NULL,
+  user_id INT AUTO_INCREMENT,
+  username VARCHAR(50) PRIMARY KEY,
   password VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL,
   country VARCHAR(50),
   top_genre VARCHAR(50),
   date_user_created DATE DEFAULT CURRENT_DATE
 );
+
 

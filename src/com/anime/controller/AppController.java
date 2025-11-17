c702655279c7ab95b21892031a99c6764513d3b3
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-17 09:05:06 +0800

    add: Action listener for login buttoni

--- src/com/anime/controller/AppController.java
+++ src/com/anime/controller/AppController.java
@@ -1,15 +1,49 @@
 package com.anime.controller;
 
-import com.anime.dao.AccountDAO;
+import com.anime.model.dao.*;
+import com.anime.model.*;
+import com.anime.view.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class AppController {
-    private AccountDAO accountDAO; // Data Access Object for account operations
+    private AnimeFrame view = new AnimeFrame();
+    private AppModel model = new AppModel();
+    private Account currentSession;
+    public AppController(AnimeFrame view, AppModel model){
+        this.view = view;
+        this.model = model;
 
-    public AppController() {
-        accountDAO = new AccountDAO();
+        init_application();
     }
 
-    public boolean loginUser(String username, String password) {
-        return accountDAO.validateLogin(username, password);
+    private void init_application(){
+        init_accpnl_listeners();
     }
+
+    private void init_accpnl_listeners() {
+        AccountPanel login = view.getLoginPanel();
+
+        login.getLoginBtn().addActionListener(e->{
+            String username = login.getLoginName();
+            String pw = login.getLoginPassword();
+            if(username.isEmpty() || pw.isEmpty()){
+                System.out.println("Username and password cannot be empty");
+                return;
+            }
+            try {
+                currentSession = model.getAccountDAO().selectAccountByUsername(username,pw);
+
+                if(currentSession!=null){
+                    System.out.println("Login Success");
+                    view.switchView(view.HOME);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
 }

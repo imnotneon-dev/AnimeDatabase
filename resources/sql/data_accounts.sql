8b16364d62946cf8b5e89fbfe4c4734d92f337fd
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-18 13:48:41 +0800

    Update data_accounts.sql

--- resources/sql/data_accounts.sql
+++ resources/sql/data_accounts.sql
@@ -11,33 +11,18 @@ CREATE TABLE Users(
 -- INSERTS
 
 INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('thistlezhi', 'zh15p4ssword', '2006-08-21', 'China', 'Action');
+VALUES 
 
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('cerizz_', 'xLuvr8', '2014-06-16', 'Philippines', 'Comedy');
+('thistlezhi', 'zh15p4ssword', '2006-08-21', 'China', 'Action');
+('cerizz_', 'xLuvr8', '2014-06-16', 'Philippines', 'Comedy');
+('_zafibleh', 'm3oW!', '2005-10-28', 'Canada', 'Romance');
+('icep0p', 'xD_rul3z', '2006-07-27', 'South Korea', 'Horror');
+('Mother_Coco', 'AFDG2028.switz!', '2004-12-18', 'Switzerland', 'Ecchi');
+('IJ.Orts', '1ts_IJcUhh', '2005-06-29', 'Taipei', 'Supernatural');
+('Azii95', 'k4tch0W!', '2005-08-14', 'America', 'Fantasy');
+('YSA', 'gl@ssy!!', '2012-08-10', 'Japan', 'Drama');
+('SMHMyHead', 'r4ndOm_p@ssw0rd!', '2006-10-06', 'Japan', 'Adventure');
+('RYAN12345801', 's3cret!S4NT4', '2007-12-05', 'Philippines', 'Sci-Fi');
 
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('_zafibleh', 'm3oW!', '2005-10-28', 'Canada', 'Romance');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('icep0p', 'xD_rul3z', '2006-07-27', 'South Korea', 'Horror');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('Mother_Coco', 'AFDG2028.switz!', '2004-12-18', 'Switzerland', 'Ecchi');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('IJ.Orts', '1ts_IJcUhh', '2005-06-29', 'Taipei', 'Supernatural');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('Azii95', 'k4tch0W!', '2005-08-14', 'America', 'Fantasy');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('SMHMyHead', 'r4ndOm_p@ssw0rd!', '2006-10-06', 'Japan', 'Adventure');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('aridesu', 'gl@ssy!!', '2012-08-10', 'Romania', 'Drama');
-
-INSERT INTO Users (username, password, date_of_birth, country, top_genre)
-VALUES ('RYAN12345801', 's3cret!S4NT4', '2007-12-05', 'Philippines', 'Sci-Fi');
 
 

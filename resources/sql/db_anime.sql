5ef2840a6d64fe830673fe8f6b5387b0503211de
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-19 07:55:56 +0800

    update: Updated sql schema - Has user_id as PK - top_genre is by default None (will be updated in the app)

--- resources/sql/db_anime.sql
+++ resources/sql/db_anime.sql
@@ -3,30 +3,30 @@ USE anime_db;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE Users(
-	user_id INT AUTO_INCREMENT,
-	username VARCHAR(50) PRIMARY KEY,
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50) NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	date_of_birth DATE NOT NULL,
-	country VARCHAR(50),
-	top_genre VARCHAR(50),
+	country VARCHAR(50) NOT NULL,
+	top_genre VARCHAR(50) DEFAULT 'N/A',
 	date_user_created DATE DEFAULT CURRENT_DATE,
 	status VARCHAR(20) DEFAULT 'Active' --for archives
 );
 
 -- INSERTS FOR USERS
---INSERT INTO Users (username, password, date_of_birth, country, top_genre)
+--INSERT INTO Users (username, password, date_of_birth, country)
 --VALUES 
 
---('thistlezhi', 'zh15p4ssword', '2006-08-21', 'China', 'Action'),
+--('thistlezhi', 'zh15p4ssword', '2006-08-21', 'Malaysia' ),
 --('cerizz_', 'xLuvr8', '2014-06-16', 'Philippines', 'Comedy'),
---('_zafibleh', 'm3oW!', '2005-10-28', 'Canada', 'Romance'),
---('icep0p', 'xD_rul3z', '2006-07-27', 'South Korea', 'Horror'),
---('Mother_Coco', 'AFDG2028.switz!', '2004-12-18', 'Switzerland', 'Ecchi'),
---('IJ.Orts', '1ts_IJcUhh', '2005-06-29', 'Taipei', 'Supernatural'),
---('Azii95', 'k4tch0W!', '2005-08-14', 'America', 'Fantasy'),
---('YSA', 'gl@ssy!!', '2012-08-10', 'Japan', 'Drama'),
---('SMHMyHead', 'r4ndOm_p@ssw0rd!', '2006-10-06', 'Japan', 'Adventure'),
---('RYAN12345801', 's3cret!S4NT4', '2007-12-05', 'Philippines', 'Sci-Fi');
+--('_zafibleh', 'm3oW!', '2005-10-28', 'Singapore', 'Romance'),
+--('icep0p', 'xD_rul3z', '2006-07-27', 'Philippines', 'Horror'),
+--('Mother_Coco', 'AFDG2028.switz!', '2004-12-18', 'Philippines'),
+--('IJ.Orts', '1ts_IJcUhh', '2005-06-29', 'Malaysia'),
+--('Azii95', 'k4tch0W!', '2005-08-14', 'Malaysia'),
+--('YSA', 'gl@ssy!!', '2012-08-10', 'Singapore'),
+--('SMHMyHead', 'r4ndOm_p@ssw0rd!', '2006-10-06', 'Malaysia'),
+--('RYAN12345801', 's3cret!S4NT4', '2007-12-05', 'Philippines');
 
 DROP TABLE IF EXISTS series;
 CREATE TABLE series (

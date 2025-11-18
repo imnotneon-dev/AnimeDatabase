41a865adc49abf066a84237dba39102e09e4a95c
Kashiragi <185775844+Kashiragi@users.noreply.github.com>
2025-11-18 20:03:43 +0800

    fix: getActorsbyEpisode->getActorsBySeries

--- src/com/anime/model/dao/ActorDAO.java
+++ src/com/anime/model/dao/ActorDAO.java
@@ -2,6 +2,7 @@ package com.anime.model.dao;
 
 import com.anime.model.Actor;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,17 +46,16 @@ public class ActorDAO {
         return roles;
     }
 
-    public List<Actor> getActorsByEpisode(int epId) throws SQLException {
+    public List<Actor> getActorsBySeries(int series_id) throws SQLException {
         StringBuilder sql = new StringBuilder();
-        sql.append("SELECT a.actor_id, a.name, a.role ");
+        sql.append("SELECT a.actor_id, a.last_name, a.first_name a.gender, a.date_of_birth, a.place_of_birth, a.agency");
         sql.append("FROM actors a ");
         sql.append("JOIN actor_series acs ON a.actor_id = acs.actor_id ");
         sql.append("JOIN series s ON acs.series_id = s.series_id ");
-        sql.append("WHERE s.ep_id = ?;");
-        
+        sql.append("WHERE s.series_id = ? LIMIT 5;");
         try(Connection conn = DBConnection.getConnection();
             PreparedStatement ps = conn.prepareStatement(sql.toString())) {
-            ps.setInt(1, epId);
+            ps.setInt(1, series_id);
             ResultSet rs = ps.executeQuery();
             List<Actor> actors = new ArrayList<>();
             while(rs.next()) {
@@ -65,7 +65,7 @@ public class ActorDAO {
                     rs.getString("first_name"),
                     rs.getString("gender"),
                     rs.getDate("date_of_birth").toLocalDate(),
-                    rs.getString("pob"),
+                    rs.getString("place_of_birth"),
                     rs.getString("agency")
                 );
                 actors.add(actor);

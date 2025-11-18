fb31cfe1216ef5299d85f19d831efe2eeaa6cae1
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-19 00:36:04 +0800

    added status in users table

--- resources/sql/db_anime.sql
+++ resources/sql/db_anime.sql
@@ -3,13 +3,14 @@ USE anime_db;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE Users(
-  user_id INT AUTO_INCREMENT,
-  username VARCHAR(50) PRIMARY KEY,
-  password VARCHAR(255) NOT NULL,
-  date_of_birth DATE NOT NULL,
-  country VARCHAR(50),
-  top_genre VARCHAR(50),
-  date_user_created DATE DEFAULT CURRENT_DATE
+	user_id INT AUTO_INCREMENT,
+	username VARCHAR(50) PRIMARY KEY,
+	password VARCHAR(255) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	country VARCHAR(50),
+	top_genre VARCHAR(50),
+	date_user_created DATE DEFAULT CURRENT_DATE,
+	status VARCHAR(20) DEFAULT 'Active' --for archives
 );
 
 -- INSERTS FOR USERS

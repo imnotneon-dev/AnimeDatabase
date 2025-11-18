9955662dedf49a1954531a2907f334b58ac5b169
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-18 13:56:41 +0800

    use user_id instead of username, add delete and select based on account

--- resources/sql/data_likedepisodes.sql
+++ resources/sql/data_likedepisodes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE LikedEpisode(
-  username VARCHAR(50),
+  user_id INT,
   episode_id INT,
   date_added DATE DEFAULT CURRENT_DATE,
   PRIMARY KEY (user_id, episode_id),
@@ -7,4 +7,20 @@ CREATE TABLE LikedEpisode(
   FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id) --episode_id variable name depends on what variable name will be used in the episodes.sql file
 );
 
+
+
+-- Delete function
+DELETE FROM LikedEpisode
+WHERE user_id = ? AND episode_id = ?;
+
+
+-- Select based on the account
+SELECT 
+    le.episode_id,
+    e.title,
+    le.date_added
+FROM LikedEpisode le
+JOIN Episodes e ON le.episode_id = e.episode_id
+WHERE le.user_id = ?
+ORDER BY le.date_added DESC;
 

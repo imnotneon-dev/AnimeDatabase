be1c42bea8d9b2e337c9b92c8a2f406716bb5e29
LilianV <rhaizza_legaspi@dlsu.edu.ph>
2025-11-19 20:16:14 +0800

    COLOR CHANGES

--- src/com/anime/view/ManageActorPanel.java
+++ src/com/anime/view/ManageActorPanel.java
@@ -23,212 +23,133 @@ public class ManageActorPanel extends JPanel {
     private JButton updateBtn = new JButton("Update");
     private JButton clearBtn = new JButton("Clear");
 
-    /*private List<String> actorList = List.of(
-            "Series 1","Series 2","Series 3","Series 4","Series 5",
-            "Series 6","Series 7","Series 8","Series 9","Series 10",
-            "Series 11","Series 12");*/
     private List<Actor> actorList = new ArrayList<>();
     private List<PlainActorCard> actorCards = new ArrayList<>();
 
     private JPanel actorListPnl = new JPanel();
 
-    public ManageActorPanel(){
+    public ManageActorPanel() {
         setLayout(new BorderLayout());
-        setBackground(Color.yellow);
+        setBackground(Color.decode("#121212")); // main background
         setBorder(new EmptyBorder(10,10,10,10));
         setupActorPanel();
     }
-    private void setupActorPanel(){
+
+    private void setupActorPanel() {
         JPanel actorListPnl = new JPanel();
         JPanel actorFormPnl = new JPanel();
         JScrollPane actorListScrollPanel = new JScrollPane(actorListPnl);
 
-        actorListPnl.setLayout(new BoxLayout(actorListPnl, BoxLayout.Y_AXIS));
-        actorListPnl.setPreferredSize(new Dimension((int)(1280/2), 720));
-        actorListPnl.setMaximumSize(new Dimension((int)(1280/2), 720));
-        actorListPnl.setBorder(new EmptyBorder(10,10,10,10));
-        actorListPnl.setBackground(Color.magenta);
-        loadPACards();
 
+        actorListPnl.setLayout(new BoxLayout(actorListPnl, BoxLayout.Y_AXIS));
+        actorListPnl.setPreferredSize(new Dimension(640, 720));
+        actorListPnl.setBackground(Color.decode("#282828")); // card background color
+        actorListPnl.setBorder(new EmptyBorder(10,10,10,10));
+        loadPACards();
 
         actorListScrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         actorListScrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         actorListScrollPanel.setWheelScrollingEnabled(true);
-        actorListScrollPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        actorListScrollPanel.getVerticalScrollBar().setUnitIncrement(10);
-        actorListScrollPanel.getVerticalScrollBar().setPreferredSize(new Dimension(3,Integer.MAX_VALUE));
+        actorListScrollPanel.setBorder(null);
         add(actorListScrollPanel, BorderLayout.WEST);
 
-        firstNameField.setMaximumSize(new Dimension(350,30));
-        firstNameField.setAlignmentX(LEFT_ALIGNMENT);
-        lastNameField.setMaximumSize(new Dimension(350,30));
-        lastNameField.setAlignmentX(LEFT_ALIGNMENT);
 
-        sexCb = new JComboBox<>(new String[] {"Male", "Female", "Other"});
+        applyFieldStyle(firstNameField);
+        applyFieldStyle(lastNameField);
+        applyFieldStyle(dobField);
+        applyFieldStyle(pobField);
+        applyFieldStyle(agencyField);
+
+        sexCb.setBackground(Color.decode("#121212"));
+        sexCb.setForeground(Color.decode("#FFFFFF"));
         sexCb.setMaximumSize(new Dimension(350,30));
-        sexCb.setAlignmentX(Component.LEFT_ALIGNMENT);
+        sexCb.setBorder(BorderFactory.createLineBorder(Color.decode("#C8C8C8")));
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        dateFormat.setLenient(false);
-        dobField = new JFormattedTextField(dateFormat);
-        dobField.setMaximumSize(new Dimension(350,30));
-        dobField.setAlignmentX(LEFT_ALIGNMENT);
 
-        pobField.setMaximumSize(new Dimension(350,30));
-        pobField.setAlignmentX(LEFT_ALIGNMENT);
+        styleButton(addBtn);
+        styleButton(updateBtn);
+        styleButton(clearBtn);
 
-        agencyField.setMaximumSize(new Dimension(350,30));
-        agencyField.setAlignmentX(LEFT_ALIGNMENT);
-
-        addBtn.setPreferredSize(new Dimension(200,35));
-        addBtn.setMaximumSize(new Dimension(200,35));
-        addBtn.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        updateBtn.setPreferredSize(new Dimension(200,35));
-        updateBtn.setMaximumSize(new Dimension(200,35));
-        updateBtn.setAlignmentX(Component.LEFT_ALIGNMENT);
         updateBtn.setEnabled(false);
 
-        actorFormPnl.add(Box.createVerticalStrut(10));
-        actorFormPnl.add(new JLabel("Enter First Name"));
+
+        actorFormPnl.setLayout(new BoxLayout(actorFormPnl, BoxLayout.Y_AXIS));
+        actorFormPnl.setPreferredSize(new Dimension(640, 720));
+        actorFormPnl.setBackground(Color.decode("#282828")); // form background
+        actorFormPnl.setBorder(new EmptyBorder(10,10,10,10));
+
+
+        actorFormPnl.add(label("Enter First Name"));
         actorFormPnl.add(firstNameField);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
-        actorFormPnl.add(new JLabel("Enter Last Name"));
+        actorFormPnl.add(label("Enter Last Name"));
         actorFormPnl.add(lastNameField);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
-        actorFormPnl.add(new JLabel("Select Sex"));
+        actorFormPnl.add(label("Select Sex"));
         actorFormPnl.add(sexCb);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
-        actorFormPnl.add(new JLabel("Enter Date of Birth"));
+        actorFormPnl.add(label("Enter Date of Birth"));
         actorFormPnl.add(dobField);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
-        actorFormPnl.add(new JLabel("Enter Place of Birth"));
+        actorFormPnl.add(label("Enter Place of Birth"));
         actorFormPnl.add(pobField);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
-        actorFormPnl.add(new JLabel("Enter Agency"));
+        actorFormPnl.add(label("Enter Agency"));
         actorFormPnl.add(agencyField);
-        actorFormPnl.add(Box.createVerticalStrut(10));
-//        actorFormPnl.add(Box.createVerticalGlue());
+
+        actorFormPnl.add(Box.createVerticalStrut(20));
         actorFormPnl.add(addBtn);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
-//        actorFormPnl.add(Box.createVerticalGlue());
         actorFormPnl.add(updateBtn);
+
         actorFormPnl.add(Box.createVerticalStrut(10));
         actorFormPnl.add(clearBtn);
+
         actorFormPnl.add(Box.createVerticalGlue());
-        actorFormPnl.setLayout(new BoxLayout(actorFormPnl,BoxLayout.Y_AXIS));
-        actorFormPnl.setPreferredSize(new Dimension((int)(1280/2), 720));
-        actorFormPnl.setMaximumSize(new Dimension((int)(1280/2), 720));
-        actorFormPnl.setBorder(new EmptyBorder(10,10,10,10));
-        actorFormPnl.setBackground(Color.orange);
-
-        add(actorFormPnl,BorderLayout.CENTER);
-
+        add(actorFormPnl, BorderLayout.CENTER);
     }
-    private void loadPACards(){
+
+    private JLabel label(String text) {
+        JLabel lbl = new JLabel(text);
+        lbl.setForeground(Color.decode("#FFFFFF"));
+        return lbl;
+    }
+
+    private void applyFieldStyle(JTextField f) {
+        f.setMaximumSize(new Dimension(350,30));
+        f.setBackground(Color.decode("#121212"));
+        f.setForeground(Color.decode("#FFFFFF"));
+        f.setCaretColor(Color.decode("#FFFFFF"));
+        f.setBorder(BorderFactory.createLineBorder(Color.decode("#C8C8C8")));
+        f.setAlignmentX(LEFT_ALIGNMENT);
+    }
+
+    private void styleButton(JButton b) {
+        b.setForeground(Color.decode("#FFFFFF"));
+        b.setBackground(Color.decode("#121212"));
+        b.setBorder(BorderFactory.createLineBorder(Color.decode("#C8C8C8")));
+        b.setFocusPainted(false);
+        b.setMaximumSize(new Dimension(200,35));
+        b.setAlignmentX(LEFT_ALIGNMENT);
+    }
+
+    private void loadPACards() {
         actorListPnl.removeAll();
         actorCards.clear();
-        for(Actor e: actorList){
+        for (Actor e : actorList) {
             PlainActorCard pac = new PlainActorCard(e.getLastName() + ", " + e.getFirstName());
             pac.putClientProperty("actor_id", e.getId());
-            actorListPnl.setAlignmentX(Component.LEFT_ALIGNMENT);
             actorCards.add(pac);
             actorListPnl.add(pac);
             actorListPnl.add(Box.createVerticalStrut(5));
         }
-
         actorListPnl.revalidate();
         actorListPnl.repaint();
     }
-
-    public JTextField getFirstNameField() {
-        return firstNameField;
-    }
-
-    public JTextField getLastNameField(){
-        return lastNameField;
-    }
-
-    public JComboBox<String> getSexCb() {
-        return sexCb;
-    }
-
-    public void setSexCb(JComboBox<String> sexCb) {
-        this.sexCb = sexCb;
-    }
-
-    public JTextField getDobField() {
-        return dobField;
-    }
-
-    public void setDobField(JTextField dobField) {
-        this.dobField = dobField;
-    }
-
-    public JTextField getPobField() {
-        return pobField;
-    }
-
-    public void setPobField(JTextField pobField) {
-        this.pobField = pobField;
-    }
-
-    public JTextField getAgencyField() {
-        return agencyField;
-    }
-
-    public void setAgencyField(JTextField agencyField) {
-        this.agencyField = agencyField;
-    }
-
-    public JButton getAddBtn() {
-        return addBtn;
-    }
-
-    public void setAddBtn(JButton addBtn) {
-        this.addBtn = addBtn;
-    }
-
-    public JButton getUpdateBtn() {
-        return updateBtn;
-    }
-
-    public void setUpdateBtn(JButton updateBtn) {
-        this.updateBtn = updateBtn;
-    }
-
-    public JButton getClearBtn() {
-        return clearBtn;
-    }
-
-    public void setClearBtn(JButton clearBtn) {
-        this.clearBtn = clearBtn;
-    }
-
-    public List<Actor> getActorList() {
-        return actorList;
-    }
-
-    public void setActorList(List<Actor> actorList) {
-        this.actorList = actorList;
-        loadPACards();
-    }
-
-    public List<PlainActorCard> getActorCards() {
-        return actorCards;
-    }
-
-    public void setActorCards(List<PlainActorCard> actorCards) {
-        this.actorCards = actorCards;
-    }
-
-    public JPanel getActorListPnl() {
-        return actorListPnl;
-    }
-
-    public void setActorListPnl(JPanel actorListPnl) {
-        this.actorListPnl = actorListPnl;
-    }
-
-
 }

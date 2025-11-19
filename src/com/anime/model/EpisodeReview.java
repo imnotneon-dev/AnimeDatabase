f0a50d5fab7b19ac90685495c1db021861b510e1
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-19 14:35:49 +0800

    Update EpisodeReview.java
    replaced comment with user_review

--- src/com/anime/model/EpisodeReview.java
+++ src/com/anime/model/EpisodeReview.java
@@ -7,14 +7,14 @@ public class EpisodeReview {
     private int review_id;
     private int user_id;
     private int episode_id;
-    private String comment;
+    private String user_review;
     private LocalDate date_reviewed; 
 
-    public EpisodeReview(int review_id, int user_id, int episode_id, String comment, LocalDate date_reviewed) {
+    public EpisodeReview(int review_id, int user_id, int episode_id, String user_review, LocalDate date_reviewed) {
         this.review_id = review_id;
         this.user_id = user_id;
         this.episode_id = episode_id;
-        this.comment = comment;
+        this.user_review = user_review;
         this.date_reviewed = date_reviewed;
     }
 
@@ -42,12 +42,12 @@ public class EpisodeReview {
         this.episode_id = episode_id;
     }
 
-    public String getComment() {
-        return comment;
+    public String getReview() {
+        return user_review;
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
+    public void setReview(String user_review) {
+        this.user_review = user_review;
     }
 
     public LocalDate getDate_reviewed() {

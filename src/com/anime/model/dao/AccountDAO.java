71600cc465d34ab74999e9a9c2fe80957578627b
thistlezhi <thistle_zhi_caindoy@dlsu.edu.ph>
2025-11-19 14:15:25 +0800

    Update AccountDAO.java
    added select account by username

--- src/com/anime/model/dao/AccountDAO.java
+++ src/com/anime/model/dao/AccountDAO.java
@@ -63,6 +63,39 @@ public class AccountDAO {
         }
     }
 
+
+    // Acc by Username
+    public Account selectAccountByUsername(String username) throws SQLException {
+        String sql = """
+            SELECT user_id, username, password, date_of_birth, country,
+                   top_genre, date_user_created, status
+            FROM Users
+            WHERE username = ? AND status = 'Active'
+        """;
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, username);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Account(
+                        rs.getInt("user_id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getDate("date_of_birth"),
+                        rs.getString("country"),
+                        rs.getString("top_genre"),
+                        rs.getDate("date_user_created"),
+                        rs.getString("status")
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
     
     // Read a user record and favorites
     /*public void viewUserWithFavorites(String username) throws SQLException {
